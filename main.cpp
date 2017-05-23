8a78c0c0a94d9687a087c87c72358386f0d4aa11
mikostan <mikostan@gmail.com>
2017-05-23 23:03:11 +0200

    old version

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+
+// Usage: ./main host port request_count thread_count
+
 #include <iostream>
 #include <unistd.h>
 #include <sys/socket.h>
@@ -6,13 +9,24 @@
 #include <sys/time.h>
 #include <pthread.h>
 #include <algorithm>
+#include <cstring>
+#include <stdio.h>
+#include <string.h>
 
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
 
-
-// Usage: ./main host port request_count thread_count
+int request_count, thread_count, requests_per_thread;
+int *response_times;
+char **arguments;
 
 void error(const char *msg) { perror(msg); exit(0); }
-
+//--------------------------------------------------------------------------------
 int check_if_header_ended(char *buffer) {
   const char *search_string1 = "\r\n\r\n";
   const char *search_string2 = "\n\r\n\r";
@@ -23,15 +37,18 @@ int check_if_header_ended(char *buffer) {
   find_buffer = strstr(buffer, search_string1);
   if (find_buffer != NULL) {
     // printf("Header ended\n");
+    return 1;
   }
   else {
     find_buffer = strstr(buffer, search_string2);
     if (find_buffer != NULL) {
       // printf("Header ended\n");
+      return 1;
     }
   }
-  return ofset;
+  return 0;
 }
+//--------------------------------------------------------------------------------
 
 int find_content_length(char *buffer) {
   const char *search_string1 = "Content-Length:";
@@ -73,25 +90,25 @@ int find_content_length(char *buffer) {
   return atoi(content_length);
 }
 
-
+//--------------------------------------------------------------------------------
 
 int make_request(char *argv[]) {
-  int portno = atoi(argv[2]);
   char *host = argv[1];
+  int portno = atoi(argv[2]);
+  // int portno = 80;
+  // char *host = "api.somesite.com";
   char *user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0";
-
-
-  char *message_format = "GET / HTTP/1.1\r\nHost: %s\r\nUser-Agent: %s\r\n\r\n";
+  char *message_format = "PUT /apikey=%s&command=%s HTTP/1.0\r\n\r\n";
   char message[65535];
 
   struct hostent *server;
   struct sockaddr_in serv_addr;
   int sockfd, bytes, sent, received, total;
-  char response_header[512];
 
   sprintf(message,message_format,host,user_agent);
 
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  //utworzenie gniazda TCP
+  sockfd = socket(PF_INET, SOCK_STREAM, 0);
   if (sockfd < 0) error("ERROR opening socket");
 
   server = gethostbyname(host);
@@ -105,71 +122,55 @@ int make_request(char *argv[]) {
   if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
     error("ERROR connecting");
 
+  //wysłanie żądania
   total = strlen(message);
   sent = 0;
   do {
     bytes = write(sockfd,message+sent,total-sent);
     if (bytes < 0)
-      error("ERROR writing message to socket");
+    error("ERROR writing message to socket");
     if (bytes == 0)
       break;
     sent+=bytes;
   } while (sent < total);
-
 
   struct timeval t1, t2;
   double elapsedTime;
 
   gettimeofday(&t1, NULL);
 
-
-
-
-  char *response_body = NULL;
   int content_length = -1;
+  int oneBajt = 1;
+  char response_header[oneBajt];
+  char whole_header[1600];
+
+  //odbiór nagłówka
+  memset(response_header, 0, sizeof(response_header));
+  memset(whole_header, 0, sizeof(whole_header));
+  // total = sizeof(whole_header)-1;
+  received = 0;
+  int licznik=0;
+  do {
+    bytes = read(sockfd, response_header, oneBajt);
+    memcpy(whole_header + licznik*oneBajt, response_header, oneBajt); licznik++;
+    // printf("bytes = %d   |   koniec=%d\n", bytes, check_if_header_ended(whole_header));
+  } while (!check_if_header_ended(whole_header) && bytes==oneBajt);
 
 
+    content_length = find_content_length(whole_header);
+    char response_body[content_length];
 
-
-  memset(response_header,0,sizeof(response_header));
-  total = sizeof(response_header)-1;
   received = 0;
   do {
-    bytes = read(sockfd, response_header, 511);
-    // printf("%s\n", response_header);
-
-    check_if_header_ended(response_header);
-    content_length = find_content_length(response_header);
-
-
-    // printf("Content length: %d\n", content_length);
-    if (content_length > 0) {
-      response_body = new char[content_length];
-    }
-
+    bytes = read(sockfd, response_body+received, content_length-received);
     if (bytes < 0)
-      error("ERROR reading response from socket");
-    if (bytes < 511 || check_if_header_ended(response_header))
+      error("ERROR reading response from socket!");
+
+    if (bytes == content_length)
       break;
     received+=bytes;
 
-  } while (1);
-
-  // printf("Reading response body\n");
-
-  do {
-    bytes = read(sockfd, response_body, content_length);
-    // printf("Received %d bytes\n", bytes);
-
-    if (bytes <= 0)
-      error("ERROR reading response from socket");
-    if (bytes < content_length)
-      break;
-    received+=bytes;
-
-  } while (1);
-
-  // printf("End read\n");
+  } while (received < content_length);
 
   gettimeofday(&t2, NULL);
 
@@ -181,78 +182,28 @@ int make_request(char *argv[]) {
 
   close(sockfd);
 
-  delete[] response_body;
+  // delete[] response_body;
 
   return elapsedTime;
 }
+//--------------------------------------------------------------------------------
 
-
-int request_count, thread_count, requests_per_thread;
-int *response_times;
-
-char **arguments;
 
 void *make_requests(void *arg) {
   int thread_start_index = *((int *)arg) * requests_per_thread;
   int thread_end_index = thread_start_index + requests_per_thread;
 
   for (int i = thread_start_index; i < thread_end_index; i++) {
+    printf("Teraz dziala watek: %d, zadanie: %d\n", *((int *)arg), i-thread_start_index);
     response_times[i] = make_request(arguments);
   }
-
   return NULL;
 }
 
+//--------------------------------------------------------------------------------
 
-int main(int argc, char *argv[]) {
-  request_count = atoi(argv[3]);
-  thread_count = atoi(argv[4]);
-
-  arguments = argv;
-
-  requests_per_thread = request_count / thread_count;
-
-  response_times = new int[thread_count * requests_per_thread];
-
-  printf("Request count: %d\n", request_count);
-  printf("Thread count: %d\n", thread_count);
-
-
-
-  pthread_t *thread_ids = new pthread_t[thread_count];
-  int *thread_indices = new int[thread_count];
-
-  struct timeval t1, t2;
-  double elapsedTime;
-
-  gettimeofday(&t1, NULL);
-
-  for (int i = 0; i < thread_count; i++) {
-    thread_indices[i] = i;
-    if (pthread_create(thread_ids + i, NULL, make_requests, thread_indices + i)) {
-      printf("error while creating thread\n");
-      return 1;
-    }
-  }
-
-  for (int i = 0; i < thread_count; i++) {
-    if (pthread_join(thread_ids[i], NULL)) {
-      printf("error while joining thread\n");
-      return 1;
-    }
-  }
-
-  gettimeofday(&t2, NULL);
-
-  elapsedTime = (t2.tv_sec - t1.tv_sec);      // sec to ms
-  elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000000.0;   // us to ms
-
-  printf("Elapsed time: %.2f s\n", elapsedTime);
-
-
-
+void calculateResult() {
   float avg_time = 0;
-
   for (int i = 0; i < thread_count * requests_per_thread; i++) {
     avg_time += response_times[i];
   }
@@ -264,11 +215,62 @@ int main(int argc, char *argv[]) {
   float min_time = response_times[0];
   float max_time = response_times[thread_count * requests_per_thread - 1];
 
-  printf("Average time: %.0f us\n", avg_time);
-  printf("Median time:  %.0f us\n", median_time);
-  printf("Min time:     %.0f us\n", min_time);
-  printf("Max time:     %.0f us\n", max_time);
+  printf("Min time:\t");
+  printf(ANSI_COLOR_GREEN "%.0f us\n" ANSI_COLOR_RESET, min_time);
+  printf("Average time:\t");
+  printf(ANSI_COLOR_YELLOW "%.0f us\n" ANSI_COLOR_RESET, avg_time);
+  printf("Max time:\t");
+  printf(ANSI_COLOR_RED "%.0f us\n" ANSI_COLOR_RESET, max_time);
+  printf("Median time:\t");
+  printf(ANSI_COLOR_MAGENTA "%.0f us\n" ANSI_COLOR_RESET, median_time);
 
+}
+
+//--------------------------------------------------------------------------------
+
+
+int main(int argc, char *argv[]) {
+  request_count = atoi(argv[3]);
+  thread_count = atoi(argv[4]);
+  arguments = argv;
+
+  requests_per_thread = request_count / thread_count;
+  response_times = new int[thread_count * requests_per_thread];
+  printf("Request count:\t");
+  printf(ANSI_COLOR_CYAN "%d\n" ANSI_COLOR_RESET, request_count);
+  printf("Thread count:\t");
+  printf(ANSI_COLOR_CYAN "%d\n" ANSI_COLOR_RESET, thread_count);
+  pthread_t *thread_ids = new pthread_t[thread_count];
+  int *thread_indices = new int[thread_count];
+
+  struct timeval t1, t2;
+  double elapsedTime;
+
+  gettimeofday(&t1, NULL);
+
+  for (int i = 0; i < thread_count; i++) {
+    thread_indices[i] = i;
+    if (pthread_create(thread_ids + i, NULL, make_requests, thread_indices + i)) {
+      printf("Error while creating thread\n");
+      return 1;
+    }
+  }
+
+  for (int i = 0; i < thread_count; i++) {
+    if (pthread_join(thread_ids[i], NULL)) {
+      printf("Error while joining thread\n");
+      return 1;
+    }
+  }
+
+  gettimeofday(&t2, NULL);
+
+  elapsedTime = (t2.tv_sec - t1.tv_sec);                  // sec to ms
+  elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000000.0;   // us to ms
+
+  printf("Elapsed time:\t%.2f s\n", elapsedTime);
+
+  calculateResult();
 
   delete[] response_times;
   delete[] thread_indices;
